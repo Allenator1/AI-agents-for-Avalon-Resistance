cfee39e23b480dc7943b768fd965e295651fbfad
Allen Antony <22706998@student.uwa.edu.au>
2021-10-13 22:30:10 +0800

    Added methods in node class for monte carlo expansion. Changed ActionMatrix class so that it can be abstracted to a regular node object (same outside behaviour)

--- MCTS/node.py
+++ MCTS/node.py
@@ -4,19 +4,18 @@ from MCTS.state import Gamestates
 
 '''
 A class used to define nodes in the Monte Carlo tree, including methods
-used for node expansion, selection and playouts
+used for node expansion, selection and playouts.
 '''
 class Node:
-    def __init__(self, state, parent=None, action=None, player=-1, team=None):
+    def __init__(self, player, parent=None, action=None):
         self.parent = parent    # None if root state
         self.action = action    # Action that parent took to lead to this node. None if root state.
-        self.player = player    # Player id; -1 if environmental state
+        self.player = player    # Player id
         self.reward = 0         # MCTS reward during backpropagation
         self.visits = 0         # MCTS visits during backpropagation
         self.avails = 0         # Number of times parent has been visited during backpropagation
-        self.state = state      # Gamestate that the node represents
-        self.child_nodes = []
-
+        self.child_nodes = {}
+           
 
     def ucb_selection(self, possible_actions, exploration):
         legal_children = filter(lambda c: c.action in possible_actions, self.child_nodes)
@@ -25,39 +24,50 @@ class Node:
         for child in legal_children:
             child.avails += 1
 
-        return max(legal_children, key=ucb_eq)
+        node = max(legal_children, key=ucb_eq)
 
-
-    def append_child(self, state, player):
-        child_node = Node(state=state, parent=self, player=player)
-        self.child_nodes.append(child_node)
-        return child_node
+        return (node, node.action)
 
     
-    def backpropagate(self, terminal_state):
+    def expand(self, next_player, possible_actions):
+        unexplored_actions = self.unexplored_actions(possible_actions)
+        if unexplored_actions != []:
+            action = random.choice(unexplored_actions)
+            node = self.append_child(next_player, action)
+            return (node, action)
+
+
+    def append_child(self, next_player, action):
+        child_node = Node(player=next_player, parent=self, action=action)
+        self.child_nodes[action] = child_node
+        return (child_node, action)
+
+    
+    def backpropagate(self, terminal_state, child_node):
         self.reward += terminal_state.get_result(self.player)
         self.visits += 1
 
 
     def unexplored_actions(self, possible_actions):
-        explored_actions = [child.action for child in self.child_nodes]
-        return filter(lambda m: m not in explored_actions, possible_actions)
+        explored_actions = list(self.child_nodes.keys())
+        return filter(lambda a: a not in explored_actions, possible_actions)
 
 
 '''
 A class used to define nodes in the Monte Carlo tree, where the game state
-contains simultaneous actions. Environmental node.
+contains simultaneous actions. Uses Decoupled UCT Selection (DUCT) to choose a
+child node during the selection phase.
 '''
 class ActionMatrix(Node):
-    def __init__(self, state, parent, action):
+    def __init__(self, player, parent, action):
         self.parent = parent
         self.action = action
-        self.state = state
-        self.child_nodes = []
+        self.player = -1
         self.player_actions = {}
+        self.child_nodes = {}
 
     
-    def decoupled_uct_selection(self, possible_actions, exploration):
+    def ucb_selection(self, possible_actions, exploration):
         legal_actions = filter(lambda a: a.action in possible_actions, self.player_actions.values())
         joint_action = [None] * len(self.player_actions)
         for p in self.player_actions:
@@ -68,35 +78,29 @@ class ActionMatrix(Node):
                 action.avails += 1
 
             selected_action = max(actions, key=ucb_eq)
-
-            if self.state.gamestate == Gamestates.VOTING:
-                joint_action[p] = selected_action
-            elif self.state.gamestate == Gamestates.MISSION:
-                joint_action.append((p, selected_action))
-        return joint_action
+            joint_action.append((p, selected_action))
+            node = self.child_nodes[joint_action]
+        return node, joint_action
 
     
-    def append_action_node(self, joint_action):
-        if joint_action['type'] == Gamestates.VOTING:
-            for p, vote in enumerate(joint_action['action']):
-                if p not in self.player_actions:
-                        self.player_actions[p] = []
-                self.player_actions[p].append(ActionNode(parent=self, player=p, action=vote))
+    def append_child(self, next_player, joint_action):
+        for p, action in enumerate(joint_action['action']):
+            if p not in self.player_actions:
+                self.player_actions[p] = []
+            self.player_actions[p].append(ActionNode(parent=self, player=p, action=action))
 
-        elif joint_action['type'] == Gamestates.MISSION:
-            for p, has_sabotaged in joint_action['action']:
-                if p not in self.player_actions:
-                    self.player_actions[p] = []
-                self.player_actions[p].append(ActionNode(parent=self, player=p, action=has_sabotaged))
-
-        else:
-            raise Exception("Initialising action matrix with non-simultaneous actions")
+        child_node = Node(player=next_player, parent=self, action=joint_action)
+        self.child_nodes[joint_action] = child_node
+        return (child_node, joint_action)
 
     
-    def unexplored_actions(self, possible_actions):
-        explored_actions = [action_node.action for action_node in self.player_actions]
-        return filter(lambda m: m not in explored_actions, possible_actions)
-
+    def backpropagate(self, terminal_state, child_node):
+        joint_action = child_node.action
+        for p, _ in enumerate(joint_action['action']):
+            action_node = self.player_actions[p]
+            action_node.reward += terminal_state.get_result(p)
+            action_node.visits += 1
+    
 
 class ActionNode():
     def __init__(self, parent, player, action):
@@ -112,7 +116,7 @@ class ActionNode():
         self.visits += 1
 
 
-def rollout(state):
+def playout(state):
     while state.get_moves() != []:
         state.make_move(random.choice(state.get_moves()))
     return state
