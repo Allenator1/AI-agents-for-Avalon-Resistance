787af8fb8a03db922efb6cbee587b908022535be
Allen Antony <22706998@student.uwa.edu.au>
2021-10-18 18:50:02 +0800

    Defined an action class to encapsulate data relevant to actions

--- MCTS/state.py
+++ MCTS/state.py
@@ -18,20 +18,35 @@ class StateNames():
 Stores all information pertaining to the state of the game at any point in time.
 '''
 class GameState():
-    def __init__(self, state_name, rnd, missions_succeeded, player, mission=[], num_selection_fails=0):
-        self.player = player       
+    def __init__(self, leader, player, state_name, rnd, missions_succeeded, mission=[], num_selection_fails=0):
+        self.leader = leader                                # Stores the player_id of the last leader (current leader if in SELECTION state)
+        self.player = player                                # Current player/players in the state. -1 for a terminal state.
         self.state_name = state_name                        # Defines the current game state (SELECTION, VOTING, SABOTAGE or TERMINAL)
         self.rnd = rnd
         self.missions_succeeded = missions_succeeded
         self.mission = mission                                
-        self.num_selection_fails = num_selection_fails     
+        self.num_selection_fails = num_selection_fails      # Number of times a team has been rejected in the same round (max 5)  
     
     def __eq__(self, other):
+        self.leader = other.leader
         self.state_name = other.state_name and \
         self.rnd == other.rnd and \
         self.missions_succeeded == other.missions_succeed and \
         self.num_selection_fails == other.num_selection_fails and \
         self.mission == other.mission
+
+'''
+A class to encapsulate information relevant to an action
+'''
+class Action():
+    def __init__(self, type, value, partially_observable):
+        self.type = type
+        self.value = value 
+        if type == StateNames.SABOTAGE or type == StateNames.VOTING:
+            self.is_simultaneous = True
+        else: 
+            self.is_simultaneous = False
+        self.partially_observable = partially_observable
 
 
 '''
@@ -40,13 +55,14 @@ Avalon Resistance, including methods to get all moves and apply moves.
 '''
 class ResistanceState():
     def __init__(self, determination, game_state):
-        self.player = game_state.player                                  
-        self.determination = determination                    # Array of booleans where the player is spy if value is True
-        self.state_name = game_state.state_name                # Defines the current game state (SELECTION, VOTING, SABOTAGE or TERMINAL)
-        self.rnd = game_state.rnd
-        self.missions_succeeded = game_state.missions_succeeded
+        self.leader = game_state.leader                             
+        self.player = game_state.player                                       
+        self.determination = determination                          # Array of booleans such that player 'p_id' is a spy if determination[p_id] = True
+        self.state_name = game_state.state_name                     
+        self.rnd = game_state.rnd       
+        self.missions_succeeded = game_state.missions_succeeded 
         self.mission = game_state.mission                                
-        self.num_selection_fails = game_state.num_selection_fails        
+        self.num_selection_fails = game_state.num_selection_fails             
 
 
     # Returns all possible actions from this state => A(state)
@@ -57,11 +73,11 @@ class ResistanceState():
         if self.gamestate == StateNames.SELECTION:
             mission_size = Agent.mission_sizes[num_players][self.rnd - 1]
             possible_missions = combinations(range(num_players), mission_size) 
-            actions = [{'type': StateNames.SELECTION, 'action': mission} for mission in possible_missions]
+            actions = [Action(StateNames.SELECTION, mission, False) for mission in possible_missions]
             
         elif self.gamestate == StateNames.VOTING:
-            voting_combinations = product([False, True], repeat=num_players)    # e.g., [[False, True], [True, False], [False, False], [True, True]] for 2 players
-            actions = [{'type': StateNames.VOTING, 'action': enumerate(votes)} for votes in voting_combinations]
+            voting_combinations = product([False, True], repeat=num_players) 
+            actions = [Action(StateNames.VOTING, enumerate(votes), False) for votes in voting_combinations]
 
         elif self.gamestate == StateNames.SABOTAGE:
             spies = [p for p in range(num_players) if self.determination[p]]
@@ -69,24 +85,24 @@ class ResistanceState():
 
             sabotage_combinations = product((False, True), repeat=spies_in_mission)
             sabotage_combinations = [zip(spies_in_mission, sabotages) for sabotages in sabotage_combinations]
-            actions = [{'type': StateNames.SABOTAGE, 'action': sabotages} for sabotages in sabotage_combinations]
+            actions = [Action(StateNames.SABOTAGE, sabotages, False) for sabotages in sabotage_combinations]
 
         return actions
 
     
     # Changes the game state object into a new state s' where s' = s(move)
-    def make_move(self, move):
+    def make_move(self, action):
         num_players = len(self.determination)
         spies = [p for p in range(num_players) if self.determination[p]]
 
-        if move['type'] == StateNames.SELECTION:
+        if action.type == StateNames.SELECTION:
             self.gamestate = StateNames.VOTING
             self.player = range(num_players)
-            self.mission = move['action']
+            self.mission = action.value                         # Action is a list of player ids in the mission
 
-        elif move['type'] == StateNames.VOTING:
+        elif action.type == StateNames.VOTING:
             spies_in_mission = [p for p in self.mission if p in spies]
-            player_votes = move['action']
+            player_votes = action.value                         # Action has format ((p1, action1), (p2, action2), (p3, action3), ...)
             votes = [vote for _, vote in player_votes]
             num_votes_for = sum(votes)
 
@@ -96,34 +112,43 @@ class ResistanceState():
                     self.player = spies_in_mission
                 else:
                     self.gamestate = StateNames.SELECTION
-                    self.player = (self.player + 1) % len(self.determination) # BOOKMARK #
+                    self.leader = (self.leader + 1) % len(self.determination) 
+                    self.player = self.leader
                     self.rnd += 1
                     self.missions_succeeded += 1
                 self.num_selection_fails = 0
 
             elif self.num_selection_fails < 5:
                 self.gamestate = StateNames.SELECTION
-                self.player = (self.player + 1) % len(self.determination) # BOOKMARK #
+                self.leader = (self.leader + 1) % len(self.determination) 
+                self.player = self.leader
                 self.num_selection_fails += 1
 
             else:
                 self.gamestate = StateNames.SELECTION
-                self.player = (self.player + 1) % len(self.determination) # BOOKMARK #
+                self.leader = (self.leader + 1) % len(self.determination) 
+                self.player = self.leader
                 self.num_selection_fails = 0
                 self.rnd += 1
 
-        elif move['type'] == StateNames.SABOTAGE:
-            sabotages = move['action']
+        elif action.type == StateNames.SABOTAGE:
             num_fails_required = Agent.fails_required[num_players][self.rnd]
             self.rnd += 1
+            if action.partially_observable:
+                num_sabotages = action.value[0]                 # Action has format (num_sabotages, list_of_players_in_mission)
+            else:
+                _, sabotages = zip(*action.value)               # Action has format [(s1, action1), (s2, action2), ...]
+                num_sabotages = sum(sabotages)
 
-            if len(sabotages) < num_fails_required: # BOOKMARK #
+            if num_sabotages < num_fails_required:
                 self.missions_succeeded += 1
 
             if self.rnd == 5:
                 self.gamestate = StateNames.TERMINAL
             else:
                 self.gamestate = StateNames.SELECTION
+                self.leader = (self.leader + 1) % len(self.determination) 
+                self.player = self.leader
 
 
     # Returns the reward for a player based on the current determination stored in the state
@@ -138,7 +163,7 @@ class ResistanceState():
 
     
     def get_game_state(self):
-        return GameState(self.state_name, self.rnd, self.missions_succeeded, self.player, 
+        return GameState(self.leader, self.player, self.state_name, self.rnd, self.missions_succeeded, 
             self.mission, self.num_selection_fails)
 
     
